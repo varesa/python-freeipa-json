b52e2a85a2ad9aeec02642f9c19a34a902c4dcf7
Niklas Bolander <niklas.bolander@nordnet.se>
2016-03-07 10:54:18 +0100

    Added a setup.py
    Setuptools is the standard way to package python projects.
    Here's a basic file, it can be called directly to create a source or
    binary package.
    python setup.py sdist
    python setup.py bdist
    You can even create an rpm should you like with "python setup.py bdist_rpm".
    
    MIT is the standard license for our open source stuff for now.
    
    Markdown is not supported by PyPI so the readme needs to be in
    reStructuredText to support both them and github, but I'll leave it as
    it is for now.

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import setuptools
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+with open('README.md') as f:
+    readme = f.read()
+
+setuptools.setup(
+    name='FreeIPA JSON',
+    version='0.1',
+    author='Andreas Calminder',
+    author_email='andreas.calminder@nordnet.se',
+    url='https://github.com/nordnet/python-freeipa-json',
+    description='Lightweight FreeIPA library',
+    long_description=readme,
+    license='MIT',
+    install_requires=required,
+    scripts=['ipahttp.py']
+)
